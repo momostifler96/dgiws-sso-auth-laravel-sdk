c89205f52d6ec6ceb3aec54738d6a162380fe5af
momoledev <momoledev2021@gmail.com>
2025-04-13 15:38:03 +0000

    update sso.php

--- src/config/sso.php
+++ src/config/sso.php
@@ -1,23 +1,23 @@
 <?php
 
 return [
-    'docker_container_name' => env('SSO_CONTAINER_NAME', 'auth'),
+    'docker_container_name' => env('SSO_SERVER_CONTAINER_NAME', 'auth'),
     'base_url' => env('SSO_SERVER_URL'),
     'login_url' => env('SSO_SERVER_LOGIN_URL', env('SSO_SERVER_URL')),
     'authorize_url' => env('SSO_SERVER_AUTHORIZE_URL', env('SSO_SERVER_URL') . '/oauth/authorize'),
     'token_url' => env('SSO_SERVER_TOKEN_URL', env('SSO_SERVER_URL') . '/oauth/token'),
-    'user_info_path' => env('SSO_USER_INFO_PATH', '/api/account/me'),
-    'user_info_url' => env('SSO_USER_INFO_URL', env('SSO_SERVER_URL') . env('SSO_USER_INFO_PATH', '/api/account/me')),
+    'user_info_path' => env('SSO_SERVER_USER_INFO_PATH', '/api/account/me'),
+    'user_info_url' => env('SSO_SERVER_USER_INFO_URL', env('SSO_SERVER_URL') . env('SSO_SERVER_USER_INFO_PATH', '/api/account/me')),
     'client_id' => env('SSO_SERVER_CLIENT_ID', ''),
-    'client_secret' => env('SSO_SERVER_CLINET_SECRET', null),
+    'client_secret' => env('SSO_SERVER_CLIENT_SECRET', null),
     'login_route' => [
-        'path' => env('SSO_SERVER_LOGIN_ROUTE_PATH', '/login'),
+        'path' => env('SSO_LOGIN_ROUTE_PATH', '/login'),
         'middlewares' => [],
-        'name' => env('SSO_SERVER_LOGIN_ROUTE_NAME', 'sso.login'),
+        'name' => env('SSO_LOGIN_ROUTE_NAME', 'sso.login'),
     ],
     'callback_route' => [
-        'path' => env('SSO_SERVER_CALLBACK_ROUTE_PATH', '/login/callback'),
+        'path' => env('SSO_CALLBACK_ROUTE_PATH', '/login/callback'),
         'middlewares' => [],
-        'name' => env('SSO_SERVER_CALLBACK_ROUTE_NAME', 'sso.callback'),
+        'name' => env('SSO_CALLBACK_ROUTE_NAME', 'sso.callback'),
     ],
 ];
