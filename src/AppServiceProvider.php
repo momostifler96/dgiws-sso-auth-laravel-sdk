67656893a65e0b662be31c55d6936aba1753301d
momoledev <momoledev2021@gmail.com>
2025-04-13 15:35:53 +0000

    update AppServiceProvider.php

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -13,13 +13,13 @@ class AppServiceProvider extends ServiceProvider
     {
         $router->aliasMiddleware('dgiws.sso.auth', SSOAuthenticateMiddleware::class);
         $router->aliasMiddleware('dgiws.sso.guest', SSOAuthenticateGuestMiddleware::class);
-        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
         $this->mergeConfigFrom(
-            __DIR__ . '/../config/sso.pph',
+            __DIR__ . '/config/sso.php',
             'sso'
         );
         $this->publishes([
-            __DIR__ . '/../config/sso.php' => config_path('sso.php'),
+            __DIR__ . '/config/sso.php' => config_path('sso.php'),
         ], 'dgiws-auth-laravel-sdk-config');
     }
 }
